5efe97b700ec78a42bf99b382df139c02d94bf93
David Szilagyi <szilagyidav@gmail.com>
2017-09-25 10:03:02 +0200

    Interface created

---
+++ DynamicArray/IArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicArray
+{
+    interface IArray<T>
+    {
+        void Add(T item);
+
+        void Add(T item, int index);
+
+        T Get(int index);
+
+        void Replace(T item, int index);
+
+        void Remove(int index);  
+    }
+}
